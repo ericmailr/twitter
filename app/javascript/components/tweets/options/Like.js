756ecdf00ebc06de474668a3d323a43d7b7b6f52
Eric <ericmailr@gmail.com>
2020-11-24 18:48:32 -0700

    add fetches to get likes count and post like in Like component

--- app/javascript/components/tweets/options/Like.js
+++ app/javascript/components/tweets/options/Like.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const like = (tweetId) => {
+  // DO I NEED TO FETCH WITH THIS CSRF TOKEN EVERY TIME?
   const csrf = document
     .querySelector("meta[name='csrf-token']")
     .getAttribute("content");
-  fetch("/likes", {
+  return fetch("/likes", {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -19,6 +20,7 @@ const like = (tweetId) => {
 };
 
 const getLikesCount = (tweetId) => {
+  // DO I NEED TO FETCH WITH THIS CSRF TOKEN EVERY TIME?
   const csrf = document
     .querySelector("meta[name='csrf-token']")
     .getAttribute("content");
@@ -34,18 +36,20 @@ const getLikesCount = (tweetId) => {
 
 function Like(props) {
   const [likesCount, setLikesCount] = useState([]);
-  useEffect(() => {
-    getLikesCount(props.tweetId).then((msg) => {
-      msg.json().then((parsedJson) => {
-        setLikesCount(parsedJson.likesCount);
-      });
-    });
-  });
+  useEffect(async () => {
+    let msg = await getLikesCount(props.tweetId);
+    let json = await msg.json();
+    setLikesCount(json.likesCount);
+  }, [likesCount]);
 
   return (
     <div
       onClick={() => {
-        like(props.tweetId);
+        (async () => {
+          let msg = await like(props.tweetId);
+          let json = await msg.json();
+          setLikesCount(json.likesCount);
+        })();
       }}>
       {" "}
       <svg className="tweet-options-svg" viewBox="0 0 24 24">
@@ -55,7 +59,7 @@ function Like(props) {
             d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path>
         </g>
       </svg>
-      <span>{likesCount}</span>
+      <span className="option-count">{likesCount}</span>
     </div>
   );
 }
