e8590b2d5640ac36740adfb12dc97321d3de4d3f
Eric <ericmailr@gmail.com>
2020-11-25 18:27:35 -0700

    add uniqueness validator in Like model for liker with scope: tweet_id

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,11 @@ class LikesController < ApplicationController
     #Just do all this in Tweets controller?
     def create
         @like = Like.find_by(liker_id: current_user.id, tweet_id: params[:tweet_id])
-        if (!@like)  
+        #if (!@like)  
             Like.create(liker_id: current_user.id, tweet_id: params[:tweet_id])
-        else
-            Like.destroy(@like.id)
-        end
+        #else
+        #    Like.destroy(@like.id)
+        #end
         respond_to do |format|
             msg = { :status => "ok", :message => "Success!", :likesCount => Tweet.find(params[:tweet_id]).likes.count }
             format.json  { render :json => msg } 
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ApplicationRecord
-    validates :liker_id, presence: true
+    validates :liker_id, :tweet_id, presence: true
+    validates :liker_id, uniqueness: { scope: :tweet_id }
     belongs_to :liker, class_name: "User"
     belongs_to :tweet
 end
