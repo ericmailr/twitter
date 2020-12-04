5ecef62e362fe3f44206e3d51e8e087a8040c0b6
Eric <ericmailr@gmail.com>
2020-12-03 22:02:38 -0700

    add associations to get tweet's tweeters and quote_tweeters

--- README.md
+++ README.md
@@ -90,6 +90,6 @@ https://github.com/rails/rails/pull/39341
 
 TODO NEXT:
 
-Issue: Can retweet things more than once
+Issue: Can retweet things more than once. if already retweeted, drop down to undo retweet
 
 Eventually I need to figure out my sessions, current_user. As of now, I can't logout of one and back in as another and have it show what it should.
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,6 +7,8 @@ class Tweet < ApplicationRecord
     has_many :likers, class_name: "User", through: :likes
     has_many :retweets, class_name: "Retweet", foreign_key: "quote_id", dependent: :destroy
     has_many :quote_tweets, class_name: "QuoteTweet", foreign_key: "quote_id", dependent: :destroy
+    has_many :retweeters, class_name: "User", foreign_key: :quote_id, through: :retweets, source: :tweeter
+    has_many :quote_tweeters, class_name: "User", foreign_key: :quote_id, through: :quote_tweets, source: :tweeter
 
     def Tweet.search(query)
         query = query.downcase
