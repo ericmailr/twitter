7d5b6ebac420bf009ad71e847846bab18a123086
Eric <ericmailr@gmail.com>
2021-03-25 18:59:31 -0700

    refactor ProfilePostList

--- app/javascript/components/FollowList.js
+++ app/javascript/components/FollowList.js
@@ -3,31 +3,28 @@ import PropTypes from "prop-types";
 import PostList from "./PostList";
 
 function FollowList({ follow_type, follows, user: { handle } }) {
-  let followerList = follows[follow_type];
   return (
-    <React.Fragment>
+    <div>
+      <ul className="tab-list">
+        <li
+          className={
+            follow_type === "followers_you_know" ? "selected-tab" : ""
+          }>
+          <a href={Routes.followers_you_know_path(handle)}>
+            Followers you know
+          </a>
+        </li>
+        <li className={follow_type === "followers" ? "selected-tab" : ""}>
+          <a href={Routes.followers_path(handle)}>Followers</a>
+        </li>
+        <li className={follow_type === "following" ? "selected-tab" : ""}>
+          <a href={Routes.following_path(handle)}>Following</a>
+        </li>
+      </ul>
       <div>
-        <ul className="tab-list">
-          <li
-            className={
-              follow_type === "followers_you_know" ? "selected-tab" : ""
-            }>
-            <a href={Routes.followers_you_know_path(handle)}>
-              Followers you know
-            </a>
-          </li>
-          <li className={follow_type === "followers" ? "selected-tab" : ""}>
-            <a href={Routes.followers_path(handle)}>Followers</a>
-          </li>
-          <li className={follow_type === "following" ? "selected-tab" : ""}>
-            <a href={Routes.following_path(handle)}>Following</a>
-          </li>
-        </ul>
-        <div>
-          <PostList posts={followerList} />
-        </div>
+        <PostList posts={follows[follow_type]} />
       </div>
-    </React.Fragment>
+    </div>
   );
 }
 
--- app/javascript/components/ProfilePostList.js
+++ app/javascript/components/ProfilePostList.js
@@ -2,37 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import PostList from "./PostList";
 
-function ProfilePostList(props) {
+function ProfilePostList({ posts, postTypes, user: { handle } }) {
   let postList = {};
 
-  if (props.postTypes === "tweets") {
+  if (postTypes === "tweets") {
     postList = (
       <PostList
-        posts={props.posts.filter((postHash) => {
+        posts={posts.filter((postHash) => {
           return ["tweet", "retweet", "quotetweet"].includes(postHash.postType);
         })}
       />
     );
-  } else if (props.postTypes === "with_replies") {
+  } else if (postTypes === "with_replies") {
     postList = (
       <PostList
-        posts={props.posts.filter((postHash) => {
+        posts={posts.filter((postHash) => {
           return ["tweet", "retweet", "quotetweet", "reply"].includes(
             postHash.postType
           );
         })}
       />
     );
-  } else if (props.postTypes === "media") {
+  } else if (postTypes === "media") {
     postList = (
       <div style={{ textAlign: "center", fontSize: "26px" }}>
         Under construction
       </div>
     );
-  } else if (props.postTypes === "likes") {
+  } else if (postTypes === "likes") {
     postList = (
       <PostList
-        posts={props.posts.filter((postHash) => {
+        posts={posts.filter((postHash) => {
           return ["like"].includes(postHash.postType);
         })}
       />
@@ -40,24 +40,23 @@ function ProfilePostList(props) {
   } else {
     postList = null;
   }
-
+  /* component-ize tab list? array of {tabName, pathname} */
   return (
     <div>
       <ul className="tab-list">
-        <li className={props.postTypes === "tweets" ? "selected-tab" : ""}>
-          <a href={Routes.profile_path(props.user.handle)}>Tweets</a>
+        <li className={postTypes === "tweets" ? "selected-tab" : ""}>
+          <a href={Routes.profile_path(handle)}>Tweets</a>
         </li>
-        <li
-          className={props.postTypes === "with_replies" ? "selected-tab" : ""}>
-          <a href={Routes.profile_with_replies_path(props.user.handle)}>
+        <li className={postTypes === "with_replies" ? "selected-tab" : ""}>
+          <a href={Routes.profile_with_replies_path(handle)}>
             Tweets & Replies
           </a>
         </li>
-        <li className={props.postTypes === "media" ? "selected-tab" : ""}>
-          <a href={Routes.profile_media_path(props.user.handle)}>Media</a>
+        <li className={postTypes === "media" ? "selected-tab" : ""}>
+          <a href={Routes.profile_media_path(handle)}>Media</a>
         </li>
-        <li className={props.postTypes === "likes" ? "selected-tab" : ""}>
-          <a href={Routes.profile_likes_path(props.user.handle)}>Likes</a>
+        <li className={postTypes === "likes" ? "selected-tab" : ""}>
+          <a href={Routes.profile_likes_path(handle)}>Likes</a>
         </li>
       </ul>
       {postList}
