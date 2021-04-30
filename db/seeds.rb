0c709c970f586dbda35a109d76b9676a12d340d5
Eric <ericmailr@gmail.com>
2021-04-29 22:49:15 -0700

    change some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users = User.create!([
-    {username: "douginator", handle: "doug", email: "doug@mail.com", password: "password"}, 
-    {username: "cindy500", handle: "cindy", email: "cindy@mail.com", password: "password"},
-    {username: "thatmatt", handle: "matt", email: "matt@mail.com", password: "password"},
-    {username: "Eric Miller", handle: "eric", email: "eric@mail.com", password: "password"},
+    {username: "douginator", handle: "doug", email: "doug@mail.com", password: "passwor"}, 
+    {username: "cindy500", handle: "cindy", email: "cindy@mail.com", password: "passwor"},
+    {username: "thatmatt", handle: "matt", email: "matt@mail.com", password: "passwor"},
+    {username: "Eric Miller", handle: "eric", email: "eric@mail.com", password: "passwor"},
     {username: "guest", handle: "hiring_team", email: "recruiter@mail.com", password: "password123"},
-    {username: "google_webdev", handle: "dont_be_evil", email: "google@mail.com", password: "password"},
-    {username: "microsoft", handle: "bill_in_the_flesh", email: "bill@mail.com", password: "password"},
-    {username: "pro_webdev", handle: "webdev_inc", email: "webdev@mail.com", password: "password"}
+    {username: "google_webdev", handle: "dont_be_evil", email: "google@mail.com", password: "passwor"},
+    {username: "microsoft", handle: "bill_in_the_flesh", email: "bill@mail.com", password: "passwor"},
+    {username: "pro_webdev", handle: "webdev_inc", email: "webdev@mail.com", password: "passwor"}
 ])
 
 Follow.create!([
@@ -34,17 +34,17 @@ tweets = Tweet.create!([
     {content: "okay", user_id: users[0].id},
     {content: "my food is good", user_id: users[1].id},
     {content: "i'm full", user_id: users[1].id},
-    {content: "i'm angry about politics!", user_id: users[2].id},
+    {content: "what's up", user_id: users[2].id},
     {content: "hello", user_id: users[2].id},
-    {content: "Check out the webdev curriculum I finished at https://www.theodinproject.com/paths", user_id: users[3].id},
+    {content: "Check out the webdev curriculum I finished at https://www.theodinproject.com/paths/full-stack-ruby-on-rails?", user_id: users[4].id},
     {content: "Just ate a burrito. 10/10.", user_id: users[6].id},
     {content: "This Eric Miller fella is a straight shooter.", user_id: users[7].id},
-    {content: "Looked into Single Table Inheritance and Delegated Types with Rails... weeks ago.", user_id: users[5].id},
+    {content: "Not a fan of Cloudinary's documentation... but it's okay.", user_id: users[5].id},
     {content: "Have a good day everyone!", user_id: users[7].id},
     {content: "Have feedback on my Twitter Clone or another project? I'd love to hear! Leave it below... or anywhere on this site.", user_id: users[3].id},
     {content: "Spent the day with my family and friends at Disneyland. Wish I had just spent it making websites with Eric Miller instead.", user_id: users[7].id},
     {content: "Check out this etch-a-sketch javascript app I made: https://ericmailr.github.io/etchsketch/", user_id: users[3].id},
-    {content: "One step at a time.", user_id: users[5].id},
+    {content: "123", user_id: users[5].id},
 
 ])
 
@@ -52,7 +52,7 @@ tweet_comments = Tweet.create!([
     {content: "your day is good", user_id: users[1].id, parent: tweets[0]},
     {content: "good day, doug", user_id: users[2].id, parent: tweets[0]},
     {content: "i stole it", user_id: users[2].id, parent: tweets[1]},
-    {content: "like, real angry!", user_id: users[2].id, parent: tweets[5]}
+    {content: "dawg!", user_id: users[2].id, parent: tweets[5]}
 ])
 
 retweets = Retweet.create!([
